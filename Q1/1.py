f22dbcd64eb53e39b86f2bd4ccb2b3adcf87308e
usman <usmannaveed012@gmail.com>
2025-12-01 01:23:58 -0500

    comments for day 1

--- Q1/1.py
+++ Q1/1.py
@@ -7,19 +7,16 @@ for x in lines:
     direction = x[0]
     clicks = int(x[1:])
     temp = dial
-    if direction == "R":
+    if direction == "R":# for the right side, just checks how many times it passes 100
         dial = (dial + clicks)%100
         ans+= (temp+clicks)//100
-    else:
+    else:#for the left side
         dial = (dial - clicks)%100
 
-        if((temp-clicks)<=0):
-            offset = temp if temp > 0 else 100
-            passes = 1 + (clicks - offset) // 100
+        if((temp-clicks)<=0):#checks if we are passing 0 because that is why we count
+            offset = temp if temp > 0 else 100 #setting the offset to how many clicks we need to reach 0
+            passes = 1 + (clicks - offset) // 100 #1 for the first pass and then how many more 100s we pass
             ans += passes       
-    
-
-    
 
 
 print(ans)        
